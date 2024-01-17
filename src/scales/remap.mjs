980277b39644aaeebcdb2c5fddb238568e1c9575
xivilay <xivilay@gmail.com>
2024-01-17 20:43:39 +0100

    chore: remove deprecated methods

--- src/scales/remap.mjs
+++ src/scales/remap.mjs
@@ -1,72 +1,13 @@
-import Scale from './scale.mjs';
-import { isWhiteNote } from '../chords/utils.mjs';
-
-// deprecated
-export const getScaleTransform = (scale, base) => {
-    if (scale instanceof Scale) scale = scale.intervals;
-    if (base instanceof Scale) base = base.intervals;
-
-    const length = Scale.getSum(base);
-
-    let scaleN = 0;
-    let nextBaseNoteIndex = 0;
-    let nextNoteIndex = 0;
-    const mapped = [...Array(length).keys()].reduce((acc, n) => {
-        if (n !== nextBaseNoteIndex) {
-            return acc;
-        }
-        const returnValue = nextNoteIndex - nextBaseNoteIndex;
-
-        const nextIntervalBase = parseInt(base[scaleN]);
-        const nextInterval = parseInt(scale[scaleN]);
-
-        if (!nextIntervalBase || !nextInterval) return acc;
-
-        nextBaseNoteIndex += nextIntervalBase;
-        nextNoteIndex += nextInterval;
-        scaleN++;
-        acc.push(returnValue);
-        return acc;
-    }, []);
-    if (scale.length <= base.length) return mapped;
-    // TODO add cases when scale tonics > base
-};
-
-// deprecated - use getNotesMappingFromIntervals() instead
-// octave - octave number
-// tonics - number of tonics in scale
-// shiftMap [0, -1, -1, 0, -1, -1, -1];
-export const getNotesMapping = (octave, tonics, scaleTransform = []) => {
-    const whites = [0, 2, 4, 5, 7, 9, 11];
-    const keysCount = 12;
-    return [...Array(128).keys()]
-        .filter((note) => whites.includes(note % keysCount))
-        .map((val, i, acc) => {
-            const base = octave * keysCount;
-            const num = i - acc.indexOf(base);
-            const whiteKeyIndex = num >= 0 ? num % tonics : Math.abs((Math.abs(num + 1) % tonics) - tonics + 1);
-            const whiteKeyShift = whites[whiteKeyIndex];
-            const octaveShift = Math.floor(num / tonics);
-            let shift = base + keysCount * octaveShift;
-            shift = shift + whiteKeyShift + (scaleTransform[whiteKeyIndex] || 0);
-            return [val, shift];
-        })
-        .filter(([i, n]) => n >= 0 && n < 128)
-        .reduce((acc, [i, n]) => {
-            acc[i] = n;
-            return acc;
-        }, {});
-};
+import { isWhiteNote, notesPerOctave } from '../chords/utils.mjs';
 
 export const getNotesMappingFromIntervals = (octave, intervals, isTargetNoteIncluded = isWhiteNote) => {
-    const notesInOctave = 12;
-    const baseNote = octave * notesInOctave;
+    const baseNote = octave * notesPerOctave;
     const minNote = 0;
     const maxNote = 127;
     const map = {};
     const intervalSum = intervals.reduce((sum, val) => sum + val, 0);
-    if (intervalSum < notesInOctave) {
-        intervals.push(notesInOctave - intervalSum);
+    if (intervalSum < notesPerOctave) {
+        intervals.push(notesPerOctave - intervalSum);
     }
     const tones = intervals.length;
     map[baseNote] = baseNote;
@@ -106,15 +47,4 @@ export const getNotesMappingFromIntervals = (octave, intervals, isTargetNoteIncl
     fillMapDown();
     fillMapUp();
     return map;
-};
-
-// deprecated
-export const makeGetTransformedNotes = (scale) => (noteIndex) => {
-    const scaleTransformation = getScaleTransform(scale);
-    const getTransformedNote = (noteIndex) => {
-        const noteInOctaveIndex = noteIndex % scaleTransformation.length;
-        const noteTransformation = scaleTransformation[noteInOctaveIndex];
-        if (noteTransformation === null) return null;
-        return noteIndex + noteTransformation;
-    };
 };
